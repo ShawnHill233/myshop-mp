d9a3dbc95741ae067014e6265f65f700c6c474a9
司晓翰 <a361162743@hotmail.com>
2018-01-24 15:56:23 +0800

    improve

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 
 // var NewApiRootUrl = 'http://localhost:9999/api/';
-var NewApiRootUrl = 'http://2e602764.ngrok.io/api/';
+var NewApiRootUrl = 'http://930b83d9.au.ngrok.io/api/';
 // var NewApiRootUrl = 'https://www.lixf.shop/api/';
 
 module.exports = {
